81a4ae4d7c573aa7cf03e65cca4af65388efc1d4
baishen <baishen2009@gmail.com>
2023-02-14 20:03:11 +0800

    fix

--- src/read/read_basic.rs
+++ src/read/read_basic.rs
@@ -83,9 +83,10 @@ pub fn read_buffer<T: NativeType, R: NativeReadBuf>(
         return read_uncompressed_buffer(reader, offset, length, out_buf);
     }
 
-    let byte_size = length * core::mem::size_of::<T>();
+    let val_size = core::mem::size_of::<T>();
+    let byte_size = length * val_size;
     // If there is not enough space left to store this data, allocate more space.
-    if (out_buf.capacity() - out_buf.len()) * byte_size < uncompressed_size {
+    if (out_buf.capacity() - out_buf.len()) * val_size < uncompressed_size {
         out_buf.reserve(uncompressed_size);
     }
     let out_slice = unsafe {
